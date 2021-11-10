c74dc3ebf1ab4741b5af25716e5f60e582363f1c
tkm0424 <fcp.t4ku@gmail.com>
2021-11-10 23:56:32 +0900

    Add nuxtjs/i18n

--- pages/index.vue
+++ pages/index.vue
@@ -73,6 +73,30 @@
           </v-card-text>
         </v-card>
       </v-card-text>
+            <v-card-title>
+        nuxt-i18nの検証
+      </v-card-title>
+      <v-card-text>
+        <v-simple-table dense>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th>en</th>
+                <th>ja</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(path, i) in ['signup', 'login']"
+                :key="`path-${i}`"
+              >
+                <td>{{ path }}</td>
+                <td>{{ $t(`title.${path}`) }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -85,7 +109,7 @@ export default {
     const userKeys = Object.keys(users[0] || {}) // 追加
     return { users, userKeys }
   },
-  // data () 追加
+  // data
   data () {
     return {
       colors: ['primary', 'info', 'success', 'warning', 'error', 'background'],
