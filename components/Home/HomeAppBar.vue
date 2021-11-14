d6148d0e03b4a7a8fd2f32b77bd28ae80c13d34f
tkm0424 <fcp.t4ku@gmail.com>
2021-11-15 04:41:00 +0900

    Add homeAppBar Event

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app-bar
     app
-    dark
+    :dark="!isScrollPoint"
+    :height="homeAppBarHeight"
+    :color="toolbarStyle.color"
+    :elevation="toolbarStyle.elevation"
   >
 
-    <app-logo />
+    <app-logo
+      @click.native="$vuetify.goTo('#scroll-top')"
+    />
     <v-toolbar-title>
       {{ appName }}
     </v-toolbar-title>
@@ -18,6 +23,7 @@
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
+        @click="goTo(menu.title)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
@@ -31,11 +37,51 @@ export default {
     menus: {
       type: Array,
       default: () => []
+    },
+
+    imgHeight: {
+      type: Number,
+      default: 0
     }
   },
-  data ({ $config: { appName } }) {
+
+  data ({ $config: { appName }, $store }) {
     return {
-      appName
+      appName,
+      scrollY: 0,
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  },
+
+  computed: {
+    // 500-56 =444px超えている場合、true
+    isScrollPoint () {
+      return this.scrollY > (this.imgHeight - this.homeAppBarHeight)
+    },
+    toolbarStyle () {
+      const color = this.isScrollPoint ? 'white' : 'transparent'
+      const elevation = this.isScrollPoint ? 4 : 0
+      return { color, elevation }
+    }
+  },
+
+  // Vue.new() = Vueインスタンス
+  // マウント => Vueの実行順部が完全に整った後にmountedメソッドが実行される
+  mounted () {
+    // window.addEventListenerはJSのメソッドで 'scroll'イベントを追加して、scrollを実行した時に this.onscrollというメソッドを呼び出している
+    window.addEventListener('scroll', this.onScroll)
+  },
+  // Vueインスタンスが破壊される前に実行される(addEventListenerで追加されたイベントを破棄する)
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll () {
+      // window.scrollY JSの現在のスクロールを取得できるメソッド
+      this.scrollY = window.scrollY
+    },
+    goTo (id) {
+      this.$vuetify.goTo(`#${id}`)
     }
   }
 }
---
+++ store/index.js
@@ -0,0 +1,16 @@
+
+// stateは共通の変数をおく場所(Vueファイルのdata)
+export const state = () => ({
+  styles: {
+    homeAppBarHeight: 56
+  }
+})
+
+// 産出プロパティ(Vueファイルでいうcomputed)
+export const getters = {}
+
+// stateの値を変更する場所
+export const mutations = {}
+
+// メソッド(Vueファイルでいうmethods
+export const actions = {}
