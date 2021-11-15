fcc6e2ac58eca083715da8d85f6570c75b78ed15
tkm0424 <fcp.t4ku@gmail.com>
2021-11-16 08:01:09 +0900

    Add before_login_layout_files

---
+++ components/App/AppTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-toolbar-title>
+      {{ appName }}
+    </v-toolbar-title>
+  </div>
+</template>
+
+<script>
+export default {
+  data ({ $config: { appName } }) {
+    return {
+      appName
+    }
+  }
+}
+</script>
