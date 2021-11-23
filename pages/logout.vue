9fe0223fa5ab9568d28c77bc3735df6c002a2496
tkm0424 <fcp.t4ku@gmail.com>
2021-11-23 17:04:39 +0900

    Add user account settings

---
+++ pages/logout.vue
@@ -0,0 +1,10 @@
+<script>
+
+export default {
+  // middleware => ページをレンダリングする前に実行される
+  // nuxtServerInitの後に実行される(ライフサイクルの2番目なのでtemplateタグは必要にならない(beforeCreate内などだとtemplateがないエラーが出る))
+  middleware ({ redirect }) {
+    return redirect('/')
+  }
+}
+</script>
